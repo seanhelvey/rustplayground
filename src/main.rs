5fa9642d7784ce55164176644311061b307722e0
seanhelvey <seanhelvey@Seans-MacBook-Pro-2.local>
2018-09-14 16:08:37 -0600

    Add Heroku deploy configuration

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![feature(plugin, decl_macro)]
+#![plugin(rocket_codegen)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+fn main() {
+    rocket::ignite().mount("/hello", routes![hello]).launch();
+}
